a27e8b67b8cb8157e62649575c6dacf3343f513e
Liv Gorence <liv@gorence.io>
2023-08-02 18:03:48 -0700

    Hey, it finally works!

--- artifakt-server.nix
+++ artifakt-server.nix
@@ -1,19 +1,22 @@
-{ lib, stdenv, nixpkgs, unzip, jdk17_headless }:
+{ pkgs }:
 
-import <nixpkgs>
-
-stdenv.mkDerivation rec {
+pkgs.stdenv.mkDerivation {
   pname = "artifakt-server";
   version = "0.1.0-2278";
 
-  src = nixpkgs.fetchurl {
+  src = pkgs.fetchurl {
     url = "https://git.lovelace.lgbt/liv/artifakt/-/jobs/2278/artifacts/raw/server/build/distributions/server.zip";
     sha256 = "sha256:cda20283d0040f04576e7575bdc84d0f83759419d4c1ca21f33197057882210f";
   };
 
-  buildInputs = [ jdk17_headless ];
+  buildInputs = [ pkgs.unzip pkgs.jdk17_headless ];
 
-  meta = with lib; {
+  installPhase = ''
+    mkdir -p $out
+    cp -r * $out
+  '';
+
+  meta = {
     homepage = "https://git.lovelace.lgbt/liv/artifakt";
     description = "Artifakt Server";
     #platforms = platforms.unix;
--- flake.nix
+++ flake.nix
@@ -1,27 +1,14 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.aarch64-darwin.artifakt-server =
-      let pkgs = import nixpkgs {
-        system = "aarch64-darwin";
-      };
-      in pkgs.stdenv.mkDerivation {
-        name = "artifakt-server";
-        src = pkgs.fetchzip {
-          url = "https://git.lovelace.lgbt/liv/artifakt/-/jobs/2278/artifacts/raw/server/build/distributions/server.zip";
-          sha256 = "sha256-wF0Z+/vu02u2BzQoTUIDV+zAC9qaJ87nzQxa6CEvT3Y=";
-        };
-	buildInputs = [ pkgs.jdk17_headless ];
-
-	installPhase = ''
-	  mkdir -p $out
-	  cp -r * $out
-	'';
-      };
-
-    defaultPackage.aarch64-darwin = self.packages.aarch64-darwin.artifakt-server;
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; }; in
+        rec {
+          packages.artifakt-server = import ./artifakt-server.nix { inherit pkgs; };
+          packages.default = packages.artifakt-server;
+        }
+    );
 }
