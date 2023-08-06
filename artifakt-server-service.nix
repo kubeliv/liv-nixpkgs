e8e7a3d3e14b30c7229bcc2bb84a7d48156c161d
Liv Gorence <liv@gorence.io>
2023-08-05 22:17:34 -0700

    This probably isn't necessary now?

--- artifakt-server-service.nix
+++ artifakt-server-service.nix
@@ -4,9 +4,6 @@
     enable = true;
     description = "Artifakt server";
     script = "${pkgs.artifakt-server}/bin/artifakt-server";
-    path = with pkgs; [
-      jdk17_headless
-    ];
     environment = {
       JAVA_HOME = pkgs.jdk17_headless;
     };
