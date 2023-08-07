5c82d65a049f10081c7dcfbedec20f58de458bea
Liv Gorence <liv@gorence.io>
2023-08-06 21:35:06 -0700

    Add user for running Artifakt.

--- artifakt-server-service.nix
+++ artifakt-server-service.nix
@@ -1,11 +1,21 @@
 { config, pkgs, ... }: {
 
+  users.users.artifakt = {
+    isSystemUser = true;
+    home = "/var/lib/artifakt";
+    createHome = true;
+  };
+
   systemd.services.artifakt-server = {
     enable = true;
     description = "Artifakt server";
     script = "${pkgs.artifakt-server}/bin/artifakt-server";
     environment = {
       JAVA_HOME = pkgs.jdk17_headless;
+    };
+    serviceConfig = {
+      User = artifakt;
+      Group = artifakt;
     };
     wantedBy = [ "multi-user.target" ];
   };
