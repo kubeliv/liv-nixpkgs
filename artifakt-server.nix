46a8fd8fc6d648a63b8668ca19cc7a26fa9a3590
Liv Gorence <liv@gorence.io>
2023-08-06 18:19:29 -0700

    Update to artifakt-server 0.1.0 release.

--- artifakt-server.nix
+++ artifakt-server.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation {
   pname = "artifakt-server";
-  version = "0.1.0-2278";
+  version = "0.1.0";
   exes = [
     "artifakt-server"
   ];
 
   src = fetchurl {
-    url = "https://git.lovelace.lgbt/liv/artifakt/-/jobs/2278/artifacts/raw/server/build/distributions/server.zip";
-    sha256 = "sha256:cda20283d0040f04576e7575bdc84d0f83759419d4c1ca21f33197057882210f";
+    url = "https://git.lovelace.lgbt/liv/artifakt/uploads/598ee8aa3e852c44b8ff622cb573c5ad/server.zip";
+    sha256 = "sha256:0ccb91c321f3bf0661c36449b4138a66fc98dad8a23fc60644c35a180bbd7f6e";
   };
 
   buildInputs = [ unzip jdk17_headless makeWrapper ];
