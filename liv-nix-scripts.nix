2b6d73255d92e4e0daf260639f007e859673559b
Olivia Lovelace <liv@lovelace.lgbt>
2024-08-14 18:00:10 -0700

    Switch to GitHub for liv-nix-scripts.

--- liv-nix-scripts.nix
+++ liv-nix-scripts.nix
@@ -1,14 +1,14 @@
-{ lib, stdenv, fetchurl, bash }:
+{ lib, stdenv, fetchFromGitHub, bash }:
 
 stdenv.mkDerivation rec {
   pname = "liv-nix-scripts";
   version = "0.1.0";
 
-  src = fetchurl {
-    url =
-      "https://git.lovelace.lgbt/liv/nix-scripts/-/archive/v${version}/nix-scripts-v${version}.tar.gz";
-    sha256 =
-      "sha256:02hmswdiqkn9q1rdq9rfd5pdl7b6za34c4z210nwrdl6n2ppx05r";
+  src = fetchFromGitHub {
+    owner = "kubeliv";
+    repo = "nix-scripts";
+    rev = "refs/tags/v${version}";
+    sha256 = "sha256-q+UYcmmA5FtkRDDwuFgqZNRVSaS3uA3y5lR+mAvS6u8=";
   };
 
   buildInputs = [ bash ];
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with lib; {
-    homepage = "https://git.lovelace.lgbt/liv/nix-scripts";
+    homepage = "https://github.com/kubeliv/nix-scripts";
     description = "Liv's Nix scripts";
     platforms = platforms.unix;
     license = with licenses; [ mit ];
